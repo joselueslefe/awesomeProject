59cce15a7bbb25e9eb4d232048d32719db961d83
José <joselueslefelima@gmail.com>
2025-06-19 21:47:51 -0300

    Initial commit

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var i int
+	var n, n2 string
+
+	fmt.Println("Olá, Estou aprendendo uma linguagem nova! ")
+
+	fmt.Println("Digite seu nome: ")
+	fmt.Scanln(&n, &n2)
+
+	fmt.Println("Digite sua idade: ")
+	fmt.Scan(&i)
+
+	fmt.Println("Sua idade e seu nome são:", i, "e", n, n2)
+}
